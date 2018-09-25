4120df1f8124b494a8ec992b9f94c6687fddb9bb
mrPelkin <mrpelkin@gmail.com>
2018-09-25 22:33:32 +0300

    first commit

---
+++ src/App.vue
@@ -0,0 +1,35 @@
+<template>
+  <div id="app">
+    <p class="light">Hello World</p>
+    <p class="regular">Hello World</p>
+    <p class="bold">Hello World</p>
+  </div>
+</template>
+
+<script>
+  import Vue from 'vue'
+
+export default {
+  name: 'app',
+  data () {
+    return {
+    }
+  }
+}
+</script>
+
+<style lang="less">
+  @import "assets/less/main.less";
+  p {
+    font-size: 10px;
+    &.light {
+      font-weight: 200;
+    }
+    &.regular {
+      font-weight: 400;
+    }
+    &.bold {
+      font-weight: 600;
+    }
+  }
+</style>
