f4fa50845b32b6db751e1d40fdd2d083baf48105
NikonovIgor <evilpsychol@gmail.com>
2018-10-09 15:04:49 +0300

    charts pie

--- src/components/finance-films.vue
+++ src/components/finance-films.vue
@@ -27,16 +27,25 @@
             height: 9 / 12 * 100 + '%'
           },
           title: {
-            text: false
+            text: 'Виды дохода от<br/>производства<br/>и продажи кино',
+            align: 'center',
+            verticalAlign: 'middle',
+            y: -20,
+            style: {
+              fontSize: '2.6rem',
+              fontFamily: '\'ProximaNova\', \'Arial\', sans-serif',
+              fontWeight: 900
+            }
           },
           plotOptions: {
             pie: {
-              innerSize: '80%'
+              innerSize: '80%',
+              enableMouseTracking: false
             }
           },
           series: [
             {
-              name: 'Delivered amount',
+              name: 'Виды дохода от производства и продажи кино',
               data: [
                 {
                   name: '<div class="chart-label-custom text-align-left"><i>08</i><span>DVD</span></div>',
