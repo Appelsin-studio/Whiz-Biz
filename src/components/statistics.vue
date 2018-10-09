12312ab84a7a34a0427783fd8b59d1cf1ea25fb2
NikonovIgor <evilpsychol@gmail.com>
2018-10-09 12:39:02 +0300

    charts

--- src/components/statistics.vue
+++ src/components/statistics.vue
@@ -8,6 +8,7 @@
             </p>
             <p class="roi">ROI по отношению к размеру бюджету</p>
             <p class="films">(1600 выпущенных фильмов в 2010 - 2015 гг.)</p>
+            <highcharts :options="chartOptions"></highcharts>
             <div class="graph">
                 <img src="/img/quantity-films.png" alt="">
             </div>
@@ -15,36 +16,119 @@
     </section>
 </template>
 <script>
-    export  default {
-      data () {
-        return {
+  import { Chart } from 'highcharts-vue'
 
+  export default {
+    components: {
+      highcharts: Chart
+    },
+    data() {
+      return {
+        chartOptions: {
+          chart: {},
+          title: {
+            text: false
+          },
+          xAxis: [
+            {
+              categories: ['$0-$5M', '$5-$10M', '$10-$15M', '$15-$20M', '$20-$25M', '$25-$50M', '$50-$100M', '$100-$300M']
+            }
+          ],
+          yAxis: [
+            {
+              labels: {
+                format: '{value}',
+                style: {}
+              },
+              title: {
+                text: 'Количество фильмов',
+                style: {}
+              },
+              min: 0
+            }, {
+              title: {
+                text: 'ROI',
+                style: {}
+              },
+              labels: {
+                format: '{value}',
+                style: {}
+              },
+              min: 0,
+              opposite: true
+            }
+          ],
+          tooltip: {
+            shared: true
+          },
+          legend: {
+            layout: 'vertical',
+            align: 'center',
+            x: 120,
+            verticalAlign: 'top',
+            y: 10,
+            floating: true
+          },
+          series: [
+            {
+              name: 'Количество фильмов',
+              type: 'column',
+              data: [509, 219, 161, 109, 77, 257, 142, 135],
+              tooltip: {
+                valueSuffix: ''
+              },
+              color: '#fae6e7',
+              softMin: 0,
+              dataLabels: {
+                enabled: true,
+                color: '#000000',
+                align: 'center',
+                y: 30, // 10 pixels down from the top
+                style: {
+                  fontSize: '18px',
+                  fontFamily: '\'ProximaNova\', \'Arial\', sans-serif',
+                  fontWeight: 200
+                }
+              }
+
+            }, {
+              name: 'ROI',
+              type: 'line',
+              yAxis: 1,
+              data: [2.79, 1.39, 1.87, 2.34, 1.73, 1.99, 2.25, 2.85],
+              softMin: 0,
+              tooltip: {
+                valueSuffix: ''
+              },
+              dataLabels: {
+                enabled: true,
+                color: '#000000',
+                align: 'center',
+                y: -15, // 10 pixels down from the top
+                style: {
+                  fontSize: '18px',
+                  fontFamily: '\'ProximaNova\', \'Arial\', sans-serif',
+                  fontWeight: 600
+                }
+              }
+            }]
         }
       }
     }
+  }
 </script>
 <style scoped lang="less">
     @import "~assets/less/_vars";
-
     .b-statistics {
         padding-top: 150px;
-
-        .sm-block({
-            padding-top: 120px;
-        });
-        .xs-block({
-            padding-top: 80px;
-        });
+        .sm-block({ padding-top: 120px; });
+        .xs-block({ padding-top: 80px; });
         .g-caption-section {
             margin-bottom: 110px;
             max-width: 640px;
             .line-after--left(-30px, 25%, #ccc);
-            .sm-block({
-                margin-bottom: 80px;
-            });
-            .xs-block({
-                margin-bottom: 50px;
-            });
+            .sm-block({ margin-bottom: 80px; });
+            .xs-block({ margin-bottom: 50px; });
         }
         .roi {
             margin-bottom: 10px;
