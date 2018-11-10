cd3082efa501ca233af07c9d772a35736aa84f6b
NikonovIgor <evilpsychol@gmail.com>
2018-11-10 21:54:12 +0300

    fix team

--- src/components/team.vue
+++ src/components/team.vue
@@ -10,13 +10,19 @@
                 <strong class="gradient-text">{{t('team')}}</strong>
             </h3>
             <div class="desc-wrapper">
-                <p class="description-team">{{t('When it comes to developing a business project - anyone, the most important and most important thing is working in a well-coordinated team and, of course, trust; creative thinking and much more.')}}</p>
-                <p class="description-team">{{t('The foundation of our foundation consists of creative people - professionals in their field with extensive experience in various fields of activity!')}}</p>
+                <p class="description-team">
+                    {{t('When it comes to developing a business project - anyone, the most important and most important thing is working in a well-' +
+                    'coordinated team and, of course, trust; creative thinking and much more.')}}
+                </p>
+                <p class="description-team">
+                    {{t('The foundation of our foundation consists of creative people - professionals in their field with extensive ' +
+                    'experience in various fields of activity!')}}
+                </p>
             </div>
             <div class="wrapper--team">
                 <div class="person person-12">
                     <div class="img-wrapper">
-                        <div class="img" style="background-image: url(/img/person-1.png)"></div>
+                        <div class="img" style="background-image: url(/img/person1.png)"></div>
                     </div>
                     <div class="profile-wrapper">
                         <p class="name">Грей Фредериксон</p>
@@ -26,7 +32,7 @@
                 </div>
                 <div class="person person-4">
                     <div class="img-wrapper">
-                        <div class="img" style="background-image: url(/img/person-2.png)"></div>
+                        <div class="img" style="background-image: url(/img/person2.png)"></div>
                     </div>
                     <div class="profile-wrapper">
                         <p class="name">Саламат Мухаммед-Али</p>
@@ -37,7 +43,7 @@
                 </div>
                 <div class="person person-4">
                     <div class="img-wrapper">
-                        <div class="img" style="background-image: url(/img/person-3.png)"></div>
+                        <div class="img" style="background-image: url(/img/person3.png)"></div>
                     </div>
                     <div class="profile-wrapper">
                         <p class="name">Питер Жмутский</p>
@@ -47,7 +53,7 @@
                 </div>
                 <div class="person person-4">
                     <div class="img-wrapper">
-                        <div class="img" style="background-image: url(/img/person-4.png)"></div>
+                        <div class="img" style="background-image: url(/img/person4.png)"></div>
                     </div>
                     <div class="profile-wrapper">
                         <p class="name">Динара Батырова</p>
@@ -80,7 +86,7 @@
         position: relative;
         padding-top: 95px;
         z-index: 15;
-        .md-block({ padding-top: 50px;});
+        .md-block({ padding-top: 50px; });
         .skew-block {
             .img {
                 width: 100%;
@@ -129,6 +135,7 @@
                 position: relative;
                 display: flex;
                 padding: 10px;
+                overflow: hidden;
                 &::after {
                     position: absolute;
                     content: '';
@@ -142,13 +149,11 @@
                 &.person-12 {
                     .size(12);
                     margin-bottom: 20px;
-                    background: linear-gradient(to right, #b303fe 0%, #442fe5 25%, #442fe5
-                    40%, rgba(37, 23, 141, 0.45) 60%, rgba(37, 23, 141, 0.55) 100%), #442fe5;
+                    background: @gradientPersonHorizontal;
                     .img-wrapper {
-                        margin: -10px -10px -10px -10px;
                         width: 100%;
                         max-width: 380px;
-                        .sm-block({max-width: 250px;});
+                        .sm-block({ max-width: 250px; });
                         .img {
                             background-position-y: 50px;
                             &::after {
@@ -166,46 +171,26 @@
                     .size(4);
                     .size-lg(12);
                     flex-direction: column;
-                    background: linear-gradient(to bottom, #b303fe  0%, #442fe5 40%, #442fe5 70%,  rgba(37,23,141,0.95)
-                    100%);
-                    .lg-block({
-                        flex-direction: row;
-                        margin-bottom: 20px;
-                        background: linear-gradient(to right, #b303fe 0%, #442fe5 25%, #442fe5
-                        40%, rgba(37, 23, 141, 0.45) 60%, rgba(37, 23, 141, 0.55) 100%), #442fe5;
-                    });
-                    .md-block({background: linear-gradient(to right, #b303fe 0%, #442fe5 25%, #442fe5
-                    30%, rgba(37, 23, 141, 0.45) 60%, rgba(37, 23, 141, 0.55) 100%), #442fe5;});
+                    background: @gradientPersonVertical;
+                    .lg-block({ flex-direction: row; margin-bottom: 20px; background: @gradientPersonVertical });
                     .img-wrapper {
-                        .lg-block({
-                            margin: -10px -10px -10px -10px;
-                            width: 100%;
-                            max-width: 380px;
-                        });
-                        .sm-block({max-width: 250px;});
+                        .lg-block({ margin: -10px -10px -10px -10px; width: 100%; max-width: 380px; });
+                        .sm-block({ max-width: 250px; });
                         .img {
-                            .lg-block({background-position-y: 50px;});
+                            .lg-block({ background-position-y: 50px; });
                             &::after {
                                 bottom: -20px;
-                                .lg-block({bottom: 0;});
+                                .lg-block({ bottom: 0; });
                             }
                         }
                     }
                     .profile-wrapper {
-                        .lg-block({padding-left: 0;
-                            padding-bottom: 0;
-                            align-self: center;});
+                        .lg-block({ padding-left: 0; padding-bottom: 0; align-self: center; });
                     }
                 }
                 .img-wrapper {
                     position: relative;
                     display: flex;
-                    margin: -10px -10px 0 -10px;
-                    background:  radial-gradient(circle at center, rgba(253,3,239,1) 0%, rgba(253,3,239,1) 120px,
-                    rgba(250,250,250,0) 120px, rgba(250,250,250,0) 100%);
-                    .sm-block({background:  radial-gradient(circle at center, rgba(253,3,239,1) 0%, rgba(253,3,239,1)
-                    90px,
-                    rgba(250,250,250,0) 90px, rgba(250,250,250,0) 100%);});
                     .img {
                         position: relative;
                         padding-top: 20px;
@@ -213,20 +198,18 @@
                         height: 320px;
                         background-repeat: no-repeat;
                         background-position: center;
-                        .sm-block({height: 250px; background-size: contain;});
-                        &::after {
-                            position: absolute;
-                            content: '';
-                            left: 0;
-                            right: 0;
-                            top: 0;
-                            background: radial-gradient(ellipse at center, rgba(250, 255, 253, 0) 0%, rgba(250, 255,
-                            253, 0) 42%, rgba(68, 47, 229, 0.9) 53%, #442fe5 56%, #442fe5 100%);
-                            background-size: 690px 600px;
-                            background-position: -150px -238px;
-                            background-repeat: no-repeat;
-                            .sm-block({ display: none;});
-                        }
+                        .sm-block({ height: 250px; background-size: contain; });
+                        z-index: 1;
+                    }
+                    &::after {
+                        content: "";
+                        position: absolute;
+                        top: -40px;
+                        left: 0;
+                        right: 0;
+                        height: 25px;
+                        border-radius: 50%;
+                        box-shadow: 0 5px 235px 87px @pinkLight;
                     }
                 }
                 .profile-wrapper {
@@ -240,7 +223,6 @@
                     align-items: flex-start;
                     z-index: 5;
                     .name {
-
                         padding-bottom: 23px;
                         margin-bottom: 15px;
                         font-weight: 900;
@@ -300,7 +282,7 @@
                 text-align: center;
                 .xs-block({ margin-bottom: 25px; });
                 .orange {
-                    color: #442FE5;
+                    color: #442fe5;
                     font-weight: 900;
                 }
             }
@@ -312,8 +294,8 @@
                 font-size: 20px;
                 font-weight: 200;
                 letter-spacing: 0.14rem;
-                color: #442FE5;
-                border-bottom: 1px solid #442FE5;
+                color: #442fe5;
+                border-bottom: 1px solid #442fe5;
                 .transition();
                 .md-block({ font-size: 16px; });
                 &:hover {
