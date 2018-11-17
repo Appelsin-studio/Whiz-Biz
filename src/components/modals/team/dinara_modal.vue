09ae1041778fa6bf5ff9dbcc3412b05d4740c6ce
mrPelkin <mrpelkin@gmail.com>
2018-11-17 14:03:58 +0300

    dinara modal

--- src/components/modals/team/dinara_modal.vue
+++ src/components/modals/team/dinara_modal.vue
@@ -3,6 +3,9 @@
         <div class="modal modal-team b-modal-salamat ">
             <div class="close-modal" @click="$modal.hide('dinara')" title="Close modal">×</div>
             <p class="name-person">Динара Батырова</p>
+            <p class="job">{{t('Entrepreneur, cryptoinvestor and film producer')}}</p>
+            <p class="person-desc">{{t('Dinara Batyrova, responsible for marketing and oversees the full development of the brand and franchise.')}}</p>
+            <p class="person-desc">{{t('Dinara, Ph.D., engineer. Professional activities: entrepreneur, cryptoinvestor, producer of the film project Ghost Town Projects: Architectural and construction objects, buildings and structures of Astana and Almaty.')}}</p>
         </div>
     </modal>
 </template>
